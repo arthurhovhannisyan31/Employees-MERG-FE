7be38b28097698311acffb227ec41db314621f94
arthurhovhannisyan31 <arthurhovhannisyan31@gmail.com>
2021-06-05 18:38:19 +0400

    dev update webpack-dev-server config, update deps, update notes

--- README.md
+++ README.md
@@ -7,13 +7,12 @@
 4. helpers
 
 ## Description
-Описание
-Назначение приложение - администрирование данных сотрудников.
-Реализовано разделение прав пользователей, администраторы могут создавать, добавлять, обновлять, удалять данные.
-Обычные пользователи имеют права на чтение и выгрузку отчетов.
-Реализован флоу юзера (авторизация, регистрация, восстановление пароля).
-Приложение имеет адаптивный дизайн.
+Project allows administrating employees data, like creating a profile and editing profile data.
+Also, you can control profile related records like paychecks, titles, employments.
+Project has full user flow: log in, log out, password reset.
+Users dashboard allows downloading report based on selected filters.
 
 ## Technologies
-Технологии
-Приложение написано на ts + react, стейт-менеджмент react.reducer + context. Данные по авторизации хранятся только в куках. 
+Project is written on ts + react, state managements uses react reducer + react context.
+Authorization data stored in cookies.
+All pages loaded on separate chunks on request using react lazy loading.
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,34 +1,34 @@
-const webpack = require("webpack");
-const path = require("path");
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-require("dotenv").config({path: '.env-dev'});
+const webpack = require('webpack')
+const path = require('path')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+require('dotenv').config({ path: '.env-dev' })
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
+  target: "web",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    host: process.env.APP_URL_DEV,
-    hot: true,
-    port: process.env.PORT || 3000,
-    inline: true,
-    disableHostCheck: true,
-    historyApiFallback: true,
-    watchOptions: {
-      poll: 1000,
-      ignored: ["node_modules"],
+    static: {
+      directory: path.join(__dirname, 'dist'),
+      watch: {
+        interval: 1000,
+        ignored: /node_modules/,
+      },
     },
-    contentBase: path.join(__dirname, "dist"),
+    firewall: false,
+    host: process.env.APP_URL_DEV,
+    port: process.env.PORT || 3000,
+    historyApiFallback: true,
     proxy: {
-      [process.env
-        .API_URL]: `${process.env.PROXY_URL}${process.env.API_URL}`,
+      [process.env.API_URL]: `${process.env.PROXY_URL}${process.env.API_URL}`,
     },
   },
   plugins: [
     ...common.plugins,
     new CleanWebpackPlugin(),
-    new webpack.ProgressPlugin({ percentBy: "entries" }),
+    new webpack.ProgressPlugin({ percentBy: 'entries' }),
     new webpack.HotModuleReplacementPlugin(),
   ],
-});
+})
