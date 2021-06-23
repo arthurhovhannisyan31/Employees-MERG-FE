506a4702515bbace2ecc9045370aaa5395fc22dd
arthurhovhannisyan31 <arthurhovhannisyan31@gmail.com>
2021-06-24 00:10:19 +0400

    ui-improvements extract ts-configs, webpack config files to config folder

--- configs/webpack/webpack.prod.config.js
+++ configs/webpack/webpack.prod.config.js
@@ -1,31 +1,36 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const CompressionPlugin = require('compression-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
+const path = require('path')
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack')
 const { merge } = require('webpack-merge')
 
 const common = require('./webpack.common.config')
-require('dotenv').config({ path: './configs/env/.env' })
+require('dotenv').config({ path: './configs/env/.env-dev' })
 
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        parallel: true,
-        test: /\.(ts|js)x?$/,
-        exclude: [/\/node_modules/, /\/dist/],
-      }),
-    ],
-    removeEmptyChunks: true,
-    mergeDuplicateChunks: true,
-    realContentHash: true,
+  target: 'web',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+      watch: {
+        interval: 1000,
+        ignored: /node_modules/,
+      },
+    },
+    host: process.env.APP_URL_DEV,
+    port: process.env.PORT || 3000,
+    historyApiFallback: true,
+    proxy: {
+      [process.env.API_URL]: `${process.env.PROXY_URL}${process.env.API_URL}`,
+    },
   },
   plugins: [
-    new CompressionPlugin({
-      threshold: 8192,
-      minRatio: 0.8,
-    }),
+    ...common.plugins,
+    new CleanWebpackPlugin(),
+    new webpack.ProgressPlugin({ percentBy: 'entries' }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
 })
