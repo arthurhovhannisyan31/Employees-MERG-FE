b0b6f75483d166cb77108e010b2cceb0c4a1264d
Artur Oganesian <arthurhovhannisyan31@gmail.com>
2021-12-11 04:58:34 +0400

    build-optimization fix accessibility issues

--- src/containers/Auth/types.ts
+++ src/containers/Auth/types.ts
@@ -1,6 +1,1 @@
-import { QueryProps } from 'model/common'
-import { UserInput } from 'model/generated'
-
 export type AuthQuery = 'signUp' | 'signIn'
-
-export type AuthQueryMap = Record<AuthQuery, (props: UserInput) => QueryProps>
